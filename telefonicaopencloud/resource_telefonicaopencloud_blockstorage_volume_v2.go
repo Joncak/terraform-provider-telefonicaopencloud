33dba9b31dfbb101a7a46d78164052755735eb6c
Jonathan Carrillo <jacarrillov@falabella.cl>
2018-11-23 18:10:49 -0300

    increase timeout for blockstore_volume_2

--- telefonicaopencloud/resource_telefonicaopencloud_blockstorage_volume_v2.go
+++ telefonicaopencloud/resource_telefonicaopencloud_blockstorage_volume_v2.go
@@ -25,8 +25,8 @@ func resourceBlockStorageVolumeV2() *schema.Resource {
 		},
 
 		Timeouts: &schema.ResourceTimeout{
-			Create: schema.DefaultTimeout(30 * time.Minute),
-			Delete: schema.DefaultTimeout(30 * time.Minute),
+			Create: schema.DefaultTimeout(60 * time.Minute),
+			Delete: schema.DefaultTimeout(60 * time.Minute),
 		},
 
 		Schema: map[string]*schema.Schema{
