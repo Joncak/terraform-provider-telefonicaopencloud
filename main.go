792d752f3e86d100933395b9092f6f4f96fef60a
Jonathan Carrillo <jacarrillov@falabella.cl>
2018-11-23 12:20:12 -0300

    fix path

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"github.com/Joncak/terraform-provider-telefonicaopencloud/telefonicaopencloud"
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/huawei-clouds/terraform-provider-telefonicaopencloud/telefonicaopencloud"
 )
 
 func main() {
